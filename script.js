b3372e410135b076a0c4c4c9298a6c8f802e698a
Brandon <32848135+v1st@users.noreply.github.com>
2018-04-27 12:38:07 -0400

    Add files via upload
    Update to interest calculator, fixed javascript equation to calculate the total. Plus small css tweaks

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-var total = document.getElementById("totalYears");
-var year = document.getElementById("inputs-yearSave");
-
-year.addEventListener('input', changePrice);
-
-
-var price = document.getElementById('inputs-startPrice');
+var tY = document.getElementById("totalYears");
+var y = document.getElementById("inputs-yearSave");
+var p = document.getElementById('inputs-startPrice');
 var dollars = document.getElementById('dollars');
+var r = document.getElementById("inputs-returnRate");
+var f = document.getElementById('frequency');
+var c = document.getElementById('inputs-addContribute');
 
-price.addEventListener('input', changePrice);
-
-
-var compoundF = document.getElementById('frequency');
-
-compoundF.addEventListener('change', function change(){
-  console.log(("changed " + compoundF.value));
-});
+y.addEventListener('input', changePrice);
+p.addEventListener('input', changePrice);
+r.addEventListener('input', changePrice);
+f.addEventListener('change', changePrice);
+c.addEventListener('input', changePrice);
 
 function changePrice() {
-  console.log(price.value);
-  dollars.innerText = price.value;
-
-  if (year > 0) {
-    total.innerText = year.value;
+  // Format check so we don't break the display
+  if (p.value > 999999999 || p.value === "") {
+    p.value = 0;
+  }
+  if (r.value > 50 || r === "") {
+    r.value = 0;
+  }
+  if (y.value > 100) {
+    y.value = 0;
+  }
+  if (y.value > 0 && y.value <= 100) {
+    tY.innerText = y.value;
+  }
+  if (c.value === "") {
+    c.value = 0;
+  }
+  // Calculate balance total with no interest
+  if (r.value === "" || r.value === 0) {
+    // Calculate compound interest for principal amount
+    let balance = Math.round(parseInt(p.value, 10) + (y.value * (c.value * f.value)));
+    // Update and display balance in USD formatting
+    dollars.innerText = balance.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+  } else {
+    // Calculate future value of a series 
+    let balance = Math.round(parseInt(p.value) * (Math.pow((1 + ((r.value / 100) / f.value)), (f.value * y.value))) * 100) / 100 +
+      (parseInt(c.value)) * ((Math.pow((1 + ((r.value / 100) / f.value)), (f.value * y.value)) - 1) / ((r.value / 100) / f.value));
+    // Update and display balance in USD formatting
+    dollars.innerText = balance.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
   }
 }
-
-
-
-// total = principal ( 1 + (i rate / compounds per year )) ^years
